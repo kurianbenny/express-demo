339166e976b1aec19e51dd723f1fa79fdccdade8
vishnu v <vishnu.vijayan96@gmail.com>
2015-05-05 18:50:33 +0530

    mogo db integrated

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var bodyParser=require('body-parser');
 var app = express();
-var mongoclient=require('mongodb').MongoClient;
-
+var mongoclient=require('mongodb').MongoClient
+,   format=require('util').format;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -13,8 +13,25 @@ var server = app.listen(8000,function () {
 });
 
 app.post('/add',function (req,res) {
-	// var num1 = parseInt(req.body.user);
-	// var num2 = parseInt(req.body.pwd);
+
+	mongoclient.connect('mongodb://127.0.0.1:27017/test',function(err,db){
+
+		if(err) throw err;
+		console.log("connected to database");
+
+		var num1 = req.body.email;
+		var num2 = req.body.password;
+
+		var document={username:'vishnu',password:'pass'};
+
+		db.collection('test').insert(document,{_id:1}, function(err, records) {
+				if (err) throw err;
+					console.log("Record added as "+records[0]._id);
+		});
+
+
+	});
+
 	var num1 = req.body.email;
 	var num2 = req.body.password;
 	res.send({'User':num1});
